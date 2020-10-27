dd7394235df7b2d5f781a43eaf0773ee9c73706c
Cristian Murtas <murtas.cristian22@gmail.com>
2020-10-27 10:13:23 +0100

    First and second exercises completed

--- Collision/main.lua
+++ Collision/main.lua
@@ -5,9 +5,13 @@
 -----------------------------------------------------------------------------------------
 physics = require('physics')
 physics.start()
+physics.setGravity(0,0)
+
 local plane = display.newImageRect("img/planeRed1.png",88,73)
 plane.x = 200
 plane.y = display.contentCenterY
+plane.name = "plane"
+physics.addBody(plane, "static", {density = 1.0})
 
 
 
@@ -15,39 +19,40 @@ local mountain = display.newImageRect("img/rockGrass.png",108 ,239)
 mountain.x = 700
 mountain.y = display.contentCenterY
 mountain.name = "mountain"
-physics.addBody (mountain, "static", {density = 1.0})
+mountain.speed = 3
+physics.addBody (mountain, "dinamic", {density = 1.0})
 
-local button = display.newRect(900,500,50,50)
 
-local options = {width = 44, height = 32, numFrames = 4}
-local missileSheet = graphics.newImageSheet("img/missileSheet.png", options)
+local explosionOpt = {width = 192, height = 192, numFrames = 7}
+local explosionSheet = graphics.newImageSheet("img/explosionSheet.png", explosionOpt)
+local explSequences = {name = "explosion", start = 1, count = 7, time = 1000, loopCount = 0, loopDirection = "forward"}
 
-local seqs = {
-    name = "missile",
-    start = 1,
-    count = 4,
-    time = 100,
-    loopCount = 0,
-    loopDirection = "forward"
-}
-
-local function onCollision(event)
+local function onCollision(self, event)
     if (event.phase == "began") then
-        print("began: " ..event.object1.name.. "and" ..event.object2.name)
+        local explosion = display.newSprite (explosionSheet, explSequences)
+        explosion.x = (plane.x + mountain.x) / 2
+        explosion.y = display.contentCenterY
     elseif (event.phase == "ended") then
-        display.remove(mountain)    
+          mountain:removeSelf()
+          mountain = nil
     end
 end
 
-local function shoot (event)
-    local missile = display.newSprite(missileSheet, seqs)
-    missile.x = 200
-    missile.y = display.contentCenterY
-    missile.name = "missile"
-    physics.addBody (missile, {density = 1.0})
-    missile:play()
-    local shoot = transition.to(missile, {x = 1080, time = 800})
+local function groundScroll (self, event) 
+    if (self.x < -display.contentWidth) then
+        self.x = display.contentWidth - self.speed * 2
+    else
+        self.x = self.x - self.speed
+    end
 end
 
-Runtime:addEventListener("collision", onCollision)
-button:addEventListener("tap", shoot)
+
+
+mountain.enterFrame = groundScroll
+Runtime:addEventListener("enterFrame", mountain)
+
+plane.collision = onCollision
+plane:addEventListener("collision")
+
+mountain.collision = onCollision
+mountain:addEventListener("collision")
