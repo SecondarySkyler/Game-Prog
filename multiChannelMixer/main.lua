3f9c9f80fa04e2aa4185beae5110373ea3497aa5
Cristian Murtas <murtas.cristian22@gmail.com>
2020-10-27 12:45:43 +0100

    Works, the problem was related to .wav file

--- multiChannelMixer/main.lua
+++ multiChannelMixer/main.lua
@@ -11,7 +11,7 @@ for num = 1,3 do
   volumeBar[num].x = display.contentCenterX
   volumeBar[num].anchorY = 0
   volumeBar[num].y = 100 + (num - 1) * 100
-  volumeBar[num].sound = audio.loadSound("audio/"..num..".wav")
+  volumeBar[num].sound = audio.loadSound("audio/"..num..".mp3")
   audio.play(volumeBar[num].sound, {channel = num, loops = -1})
 end 
 
@@ -21,14 +21,12 @@ for num = 1,3 do
     sliders[num].x = display.contentCenterX
     sliders[num].y = 150 + (num - 1) * 100
     sliders[num].offsetX = 0
-    sliders[num].channel = num
+    sliders[num].chn = num
 end
-
 
 
 local barLeftMargin = 65
 local barRightMargin = 255
-
 
 
 local function moveSlider(event)
@@ -46,11 +44,12 @@ local function moveSlider(event)
             slider.x = event.x - slider.offsetX
         end
         newVolume = (slider.x - 65) / 190
-        audio.setVolume(newVolume, {channel = slider.channel})
+        audio.setVolume(newVolume, {channel = slider.chn})
 
     elseif (event.phase == "ended" or event.phase == "cancelled") then
         display.currentStage:setFocus(nil)
     end
+    return true
 end
 
 
